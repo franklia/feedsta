a143540aa41af349efea78a243631fe05d8d86db
Frank Liardet <fliardet2@hotmail.com>
2016-11-22 21:07:28 +1100

    Photos layout improved but not finished

--- app/assets/javascripts/custom_jquery.js
+++ app/assets/javascripts/custom_jquery.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
 	// Set .pictures-wrapper height onload
-	var width = $('.pictures-wrapper').width();
-    $('.pictures-wrapper').css({ height : width + "px" }); 
+	var width = $('.photo-wrapper').width();
+    $('.photo-wrapper').css({ height : width + "px" }); 
 
     // Set .pictures-wrapper height on resizing of browser
     $(window).resize(function(){
-    	var width = $('.pictures-wrapper').width();
-    	$('.pictures-wrapper').css({ height : width + "px" }); 
+    	var width = $('.photo-wrapper').width();
+    	$('.photo-wrapper').css({ height : width + "px" }); 
     });
 });
 
