677a5e4467b99dc98bd960946aebfdf237a5db67
Frank Liardet <fliardet2@hotmail.com>
2016-12-06 21:02:06 +1100

    altered passwords controller

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,4 +1,5 @@
-class Users::PasswordsController < Devise::PasswordsController
+# class Users::PasswordsController < Devise::PasswordsController
+class PasswordsController < Devise::PasswordsController
   protected
     def after_resetting_password_path_for(resource)
       photos_path(resource)
